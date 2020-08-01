9f6a43d651907c7aa7cb3ff63130887851ee11ed
Brian Flannery <bflannery66@email.com>
2020-08-01 14:54:07 -0500

    post route connected to DB

--- endpoints/tasks/create.js
+++ endpoints/tasks/create.js
@@ -1,15 +1,34 @@
 'use strict';
+const pg = require('pg')
+
 
 module.exports.handler = async event => {
+    const { name, description } = event.body
+
+    const client = new pg.Client(dbConfig);
+
+    const query = "INSERT into tasks (name, description) VALUES ('new task', 'new task description');"
+
+    let result;
+
+    try {
+        await client.connect()
+        const res = await client.query(query)
+        console.log(res.rows[0])
+        result = res
+    } catch (err) {
+        result = err
+        console.log(err.stack)
+    }
+
+    await client.end()
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Response from Create Endpoint!',
-                input: event,
-            },
-            null,
-            2
-        ),
-    };
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(result),
+
+    }
 };
